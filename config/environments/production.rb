024f14172f4f2198bdd49993762fd92046d5820e
Keith Turner <turner@keithturner.com>
2016-09-20 09:45:23 -0600

    Heroku config 4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-
+=begin
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -79,7 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = {host: 'pintrested-turner.herokuapp.com'}
-
+=end
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
